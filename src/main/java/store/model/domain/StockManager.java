d90415735de4641db2a105fe5803d4284a14e282
kimsehw <seunghwan9703@gmail.com>
2024-11-08 13:35:32 +0900

    refactor(StockManager): 값 상수화 & 메소드,변수 추출

--- src/main/java/store/model/domain/StockManager.java
+++ src/main/java/store/model/domain/StockManager.java
@@ -8,15 +8,24 @@ import java.util.List;
 
 public class StockManager {
 
-    private List<Product> stock = new ArrayList<>();
+    private static final String SEPARATOR = ",";
+
+    private final List<Product> stock = new ArrayList<>();
 
     public void readProductsFrom(String productsFilePath) throws IOException {
         List<String> products = Files.readAllLines(Path.of(productsFilePath));
-        products.removeFirst();
+        products.removeFirst(); // 첫 라인(헤더) 제거
+        organizeStock(products);
+    }
+
+    private void organizeStock(List<String> products) {
         for (String productInformation : products) {
-            List<String> productData = List.of(productInformation.split(","));
-            stock.add(new Product(productData.get(0), Integer.parseInt(productData.get(1)),
-                    Integer.parseInt(productData.get(2)), productData.get(3)));
+            List<String> productData = List.of(productInformation.split(SEPARATOR));
+            String name = productData.get(0);
+            int price = Integer.parseInt(productData.get(1));
+            String promotion = productData.get(3);
+            int quantity = Integer.parseInt(productData.get(2));
+            stock.add(new Product(name, price, quantity, promotion));
         }
     }
 }
--- src/test/java/store/model/domain/StockManagerTest.java
+++ src/test/java/store/model/domain/StockManagerTest.java
@@ -12,15 +12,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class StockManagerTest {
 
+    private static final String PRODUCTS_FILE_PATH = "src/main/resources/products.md";
+    public static final int HEADER_LINE_COUNT = 1;
+
     private final StockManager stockManager = new StockManager();
 
     @DisplayName("상품 목록 불러오기 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"src/main/resources/products.md"})
+    @ValueSource(strings = {PRODUCTS_FILE_PATH})
     void readProductsTest(String productsFilePath) throws IOException {
         stockManager.readProductsFrom(productsFilePath);
         long productsCount = Files.lines(Path.of(productsFilePath)).count();
         assertThat(stockManager).extracting("stock").asInstanceOf(LIST)
-                .hasSize((int) productsCount - 1);
+                .hasSize((int) productsCount - HEADER_LINE_COUNT);
     }
 }
