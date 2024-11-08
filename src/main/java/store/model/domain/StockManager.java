d1dd9957d00f76f93f829482552258ce0f5c3998
kimsehw <seunghwan9703@gmail.com>
2024-11-08 16:31:19 +0900

    refactor(StockManager): 메소드 추출 & 불변성 보장
    promotions 을 불변 Map 으로 관리하여 프로그램 내에서 행사 목록 정보에 대한 불변성을 보장합니다.

--- src/main/java/store/model/domain/StockManager.java
+++ src/main/java/store/model/domain/StockManager.java
@@ -3,6 +3,7 @@ package store.model.domain;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,15 +43,17 @@ public class StockManager {
 
     private Map<String, Promotion> organizePromotions(List<String> promotionsInformation) {
         Map<String, Promotion> promotions = new HashMap<>();
-        for (String promotionInformation : promotionsInformation) {
-            List<String> promotionData = List.of(promotionInformation.split(","));
-            String name = promotionData.get(0);
-            int buy = Integer.parseInt(promotionData.get(1));
-            int get = Integer.parseInt(promotionData.get(2));
-            String startDate = promotionData.get(3);
-            String endDate = promotionData.get(4);
-            promotions.put(name, new Promotion(buy, get, startDate, endDate));
-        }
-        return promotions;
+        promotionsInformation.forEach(promotionInformation -> putPromotion(promotions, promotionInformation));
+        return Collections.unmodifiableMap(promotions);
+    }
+
+    private void putPromotion(Map<String, Promotion> promotions, String promotionInformation) {
+        List<String> promotionData = List.of(promotionInformation.split(","));
+        String name = promotionData.get(0);
+        int buy = Integer.parseInt(promotionData.get(1));
+        int get = Integer.parseInt(promotionData.get(2));
+        String startDate = promotionData.get(3);
+        String endDate = promotionData.get(4);
+        promotions.put(name, new Promotion(buy, get, startDate, endDate));
     }
 }
