1e13ebaa8866580d3ae45cfc80058267a57923e1
kimsehw <seunghwan9703@gmail.com>
2024-11-10 14:53:58 +0900

    feat(InputView): 입력 요청메세지 출력 및 받는 기능 구현
    각 상황에 맞는 입력 요청 메세지를 출력합니다.
    사용자의 입력을 받아 반환합니다.
    입력 기능이 추가됨에 따라 README 문서를 수정합니다.

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,37 @@
+package store.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import store.model.domain.PurchaseResponse;
+
+public class InputView {
+
+    public void showRequestMessageOf(InputViewType inputViewType) {
+        System.out.println(inputViewType.getRequestMessage());
+    }
+
+    public void showRequestMessageOf(InputViewType inputViewType, PurchaseResponse purchaseResponse) {
+        String requestMessage = messageFormat(inputViewType, purchaseResponse);
+        System.out.println(requestMessage);
+    }
+
+    private String messageFormat(InputViewType inputViewType, PurchaseResponse purchaseResponse) {
+        String message = inputViewType.getRequestMessage();
+        boolean isFreeProductRemindType = (inputViewType.equals(InputViewType.FREE_PRODUCT_REMIND));
+        if (isFreeProductRemindType) {
+            return formattingFreeProductRemindMessage(message, purchaseResponse);
+        }
+        return formattingPartialUnavailableMessage(message, purchaseResponse);
+    }
+
+    private String formattingFreeProductRemindMessage(String message, PurchaseResponse purchaseResponse) {
+        return String.format(message, purchaseResponse.getName());
+    }
+
+    private String formattingPartialUnavailableMessage(String message, PurchaseResponse purchaseResponse) {
+        return String.format(message, purchaseResponse.getName(), purchaseResponse.getRestCount());
+    }
+
+    public String getInput() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/store/view/InputViewType.java
@@ -0,0 +1,20 @@
+package store.view;
+
+public enum InputViewType {
+
+    PRODUCTS_NAME_AMOUNT("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
+    MEMBERSHIP("멤버십 할인을 받으시겠습니까? (Y/N)"),
+    FREE_PRODUCT_REMIND("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
+    PROMOTION_PARTIAL_UNAVAILABLE("현재 %s %,d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
+    CONTINUE_SHOPPING("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+
+    private final String requestMessage;
+
+    InputViewType(String requestMessage) {
+        this.requestMessage = requestMessage;
+    }
+
+    public String getRequestMessage() {
+        return this.requestMessage;
+    }
+}
