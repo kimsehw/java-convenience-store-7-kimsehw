814820101ea3fbfc4c85bfec7fd88bb6bcdc42d7
kimsehw <seunghwan9703@gmail.com>
2024-11-11 15:42:38 +0900

    feat(Receipt): 영수증 구성 정보 생성 기능 구현
    영수증을 구성하는 모든 정보를 ReceiptInformation 에 담아 관리합니다.

--- src/main/java/store/model/domain/Receipt.java
+++ src/main/java/store/model/domain/Receipt.java
@@ -1,9 +1,16 @@
 package store.model.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import store.constant.ConstantBox;
 
 public class Receipt {
+
+    private static final int ZERO = 0;
+    public static final int MAX_MEMBERSHIP_DISCOUNT = 8000;
+    public static final int MIN_MEMBERSHIP_DISCOUNT = 1000;
 
     private final List<String> names = new ArrayList<>();
     private final List<Integer> quantities = new ArrayList<>();
@@ -15,5 +22,53 @@ public class Receipt {
         quantities.add(salesData.getQuantity());
         prices.add(salesData.getPrice());
         promotionCounts.add(salesData.getPromotionCount());
+    }
+
+    public ReceiptInformation getReceiptInformation(String membershipRespond) {
+        List<Integer> amountInformation = generateAmountInformation(membershipRespond);
+        Map<String, List> salesDatas = generateSalesDatas();
+        return new ReceiptInformation(salesDatas, amountInformation);
+    }
+
+    private List<Integer> generateAmountInformation(String membershipRespond) {
+        int totalPurchasedAmount = ZERO;
+        int promotionDiscountAmount = ZERO;
+        for (int productKindIndex = ZERO; productKindIndex < names.size(); productKindIndex++) {
+            totalPurchasedAmount += quantities.get(productKindIndex) * prices.get(productKindIndex);
+            promotionDiscountAmount += promotionCounts.get(productKindIndex) * prices.get(productKindIndex);
+        }
+        int memberShipDiscountAmount = getMembershipDiscountAmount(membershipRespond, totalPurchasedAmount,
+                promotionDiscountAmount);
+        int payAmount = totalPurchasedAmount - promotionDiscountAmount - memberShipDiscountAmount;
+        return List.of(totalPurchasedAmount, promotionDiscountAmount, memberShipDiscountAmount, payAmount);
+    }
+
+    private int getMembershipDiscountAmount(String membershipRespond, int totalPurchasedAmount,
+                                            int promotionDiscountAmount) {
+        if (membershipRespond.equals(ConstantBox.CUSTOMER_RESPOND_N)) {
+            return ZERO;
+        }
+        return calculateMembershipDiscount(totalPurchasedAmount, promotionDiscountAmount);
+    }
+
+    private static int calculateMembershipDiscount(int totalPurchasedAmount, int promotionDiscountAmount) {
+        double discount = (totalPurchasedAmount - promotionDiscountAmount) * 0.3;
+        int memberShipDiscountAmount = ((int) discount / 1000) * 1000;
+        if (memberShipDiscountAmount < MIN_MEMBERSHIP_DISCOUNT) {
+            return ZERO;
+        }
+        if (memberShipDiscountAmount > MAX_MEMBERSHIP_DISCOUNT) {
+            return MAX_MEMBERSHIP_DISCOUNT;
+        }
+        return memberShipDiscountAmount;
+    }
+
+    private Map<String, List> generateSalesDatas() {
+        return Map.of(
+                "names", Collections.unmodifiableList(names),
+                "quantities", Collections.unmodifiableList(quantities),
+                "prices", Collections.unmodifiableList(prices),
+                "promotionCounts", Collections.unmodifiableList(promotionCounts)
+        );
     }
 }
